b1637503ee1e7db5b9d9cd5570aae8e282c9355b
Adrian Glos <adrian@Adrians-MBP.fritz.box>
2023-11-10 15:52:44 +0100

    Test the Data on some lrns with tuning

---
+++ learners_and_tuning.R
@@ -0,0 +1,91 @@
+library(mlr3verse)
+library(mlr3mbo)
+library(mlr3tuning)
+library(mlr3hyperband)
+library(paradox)
+library(mlr3fselect)
+data = read.csv("data/Train.csv")
+data[] <- lapply(data, function(x) {
+ if(is.character(x)) as.factor(x) else x
+})
+data_test = read.csv("data/Test.csv")
+task = as_task_regr(data, target = "Yield")
+
+instance_fs = fsi(
+ task = task,
+ learner = learner_rpart,
+ resampling = rsmp("cv", folds = 3),
+ measures = msr("regr.rmse"),
+ terminator = trm("evals", n_evals = 500)
+)
+fselector = fs("random_search", batch_size = 100)
+fselector$optimize(instance_fs)
+task$select(instance_fs$result_feature_set)
+task$feature_names
+#Base learners
+learner_ranger = lrn("regr.ranger", 
+                     num.trees = to_tune(1, 500),
+                     mtry= to_tune(1,10))
+
+learner_xgboost = lrn("regr.xgboost",
+                      booster = "gbtree",
+                      eta = to_tune(0, 1),
+                      max_depth = to_tune(1, 30),
+                      nrounds = to_tune(p_int(10, 250, tags = "budget")))
+
+learner_rpart = lrn("regr.rpart",
+                    cp  = to_tune(0.00001, 0.1))
+
+#Impute
+po_impute = po("imputeoor")
+#fencoder 
+fencoder = po("encode", affect_columns = selector_type("factor"))
+
+learner_xgboost = as_learner(fencoder %>>% learner_xgboost)
+
+learner_ranger = as_learner(po_impute %>>% learner_ranger)
+learner_xgboost = as_learner(po_impute %>>% learner_xgboost)
+learner_rpart = as_learner(po_impute %>>% learner_rpart)
+
+instance_ranger = tune(
+ tuner = tnr("mbo"),
+ task = task,
+ learner = learner_ranger ,
+ resampling = rsmp("cv", folds = 3),
+ measures = msr("regr.rmse"),
+ terminator = trm("run_time", secs = 300)
+)
+learner_ranger$param_set$values = instance_ranger$result_learner_param_vals
+autoplot(instance_ranger)
+
+instance_xgboost = tune(
+ tuner = tnr("hyperband", eta = 3),
+ task = task,
+ learner = learner_xgboost ,
+ resampling = rsmp("cv", folds = 3),
+ measures = msr("regr.rmse"),
+)
+learner_xgboost$param_set$values = instance_xgboost$result_learner_param_vals
+autoplot(instance_xgboost)
+
+instance_rpart = tune(
+ tuner = tnr("mbo"),
+ task = task,
+ learner = learner_rpart ,
+ resampling = rsmp("cv", folds = 3),
+ measures = msr("regr.rmse"),
+ terminator = trm("run_time", secs = 300)
+)
+learner_rpart$param_set$values = instance_rpart$result_learner_param_vals
+autoplot(instance_rpart)
+
+lrns = c(learner_ranger, learner_xgboost, learner_rpart)
+
+resampling = rsmp("cv", folds = 5)
+
+#Define the Benchmark Grid
+d = benchmark_grid(task = task, learner = lrns, resampling = resampling)
+#Run the Benchmark
+bmr = benchmark(design = d)
+mse = bmr$aggregate(msr("regr.rmse"))
+mse
