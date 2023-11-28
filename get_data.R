20eb3f3e5913818cc0c046c3c82c4240aaaa7db7
Adrian Glos <adrian@Adrians-MBP.fritz.box>
2023-11-28 11:06:48 +0100

    merge get_data with data_cleaning

--- get_data.R
+++ get_data.R
@@ -9,148 +9,136 @@ get_data = function(src){
  
  ## character columns to factor columns ##
  df <- df %>% mutate(across(c(District, Block, CropEstMethod,
-                              TransplantingIrrigationSource,
-                              TransplantingIrrigationPowerSource,
-                              PCropSolidOrgFertAppMethod, MineralFertAppMethod,
-                              MineralFertAppMethod.1, Harv_method, Threshing_method,
-                              Stubble_use), as.factor))
+                              TransplantingIrrigationSource, TransplantingIrrigationPowerSource,
+                              PCropSolidOrgFertAppMethod, MineralFertAppMethod, MineralFertAppMethod.1,
+                              Harv_method, Threshing_method, Stubble_use), as.factor))
  
- ## rename unnamed factor level ##
- levels(df$TransplantingIrrigationSource)[
-  levels(df$TransplantingIrrigationSource) == ""] <- "unknown"
- levels(df$TransplantingIrrigationPowerSource)[
-  levels(df$TransplantingIrrigationPowerSource) == ""] <- "unknown"
- levels(df$PCropSolidOrgFertAppMethod)[
-  levels(df$PCropSolidOrgFertAppMethod) == ""] <- "unknown"
- # no second dose applied
- ftable(df$MineralFertAppMethod.1, df$NoFertilizerAppln)
- levels(df$MineralFertAppMethod.1)[levels(df$MineralFertAppMethod.1) == ""] <- "no2ndDose"
+ # error in District, one observation in Gurua has as District Jamui instead of Gaya
+ ftable(df$District, df$Block)
+ df = df %>%
+  rows_update(tibble(ID = df$ID[df$District == "Jamui" & df$Block == "Gurua"],
+                     District = "Gaya"), by = "ID")
+ 
+ ## factor variables to dummy columns ##
+ df = df %>%
+  fastDummies::dummy_cols(c("CropEstMethod", "TransplantingIrrigationSource",
+                            "TransplantingIrrigationPowerSource", "PCropSolidOrgFertAppMethod",
+                            "MineralFertAppMethod", "MineralFertAppMethod.1", "Block"),
+                          remove_selected_columns = TRUE)
  
  ## character columns to dummy columns ##
- df <- df %>% mutate(mapply(function(x, methods_vec) {
-  output <- as.logical(methods_vec %in% x)
-  names(output) <- paste("LPM", methods_vec, sep = "_")
-  output}, strsplit(LandPreparationMethod, " "),
-  MoreArgs = list(methods_vec = unique(unlist(strsplit(LandPreparationMethod, " "))))) %>%
-   t() %>% as_tibble())
+ df = df %>%
+  fastDummies::dummy_cols(c("LandPreparationMethod", "NursDetFactor",
+                            "TransDetFactor", "OrgFertilizers", "CropbasalFerts", "FirstTopDressFert"),
+                          split = " ", remove_selected_columns = TRUE)
  
- df <- df %>% mutate(mapply(function(x, methods_vec) {
-  output <- as.logical(methods_vec %in% x)
-  names(output) <- paste("NDF", methods_vec, sep = "_")
-  output}, strsplit(NursDetFactor, " "),
-  MoreArgs = list(methods_vec = unique(unlist(strsplit(NursDetFactor, " "))))) %>%
-   t() %>% as_tibble())
+ ## create target variable ##
+ #df <- df %>% mutate(Yield_Acre = Yield / Acre)
  
- df <- df %>% mutate(mapply(function(x, methods_vec) {
-  output <- as.logical(methods_vec %in% x)
-  names(output) <- paste("TDF", methods_vec, sep = "_")
-  output}, strsplit(TransDetFactor, " "),
-  MoreArgs = list(methods_vec = unique(unlist(strsplit(TransDetFactor, " "))))) %>%
-   t() %>% as_tibble())
+ ## adjust wrong values ##
+ # set outlier values to NA except if there is an obvious way to correct them 
+ df <- df %>%
+  mutate(X1appDaysUrea = ifelse(X1appDaysUrea == 332, NA, X1appDaysUrea),
+         SeedlingsPerPit = ifelse(SeedlingsPerPit == 442, NA, SeedlingsPerPit),
+         Harv_hand_rent = ifelse(Harv_hand_rent == 60000, NA, Harv_hand_rent),
+         Ganaura = ifelse(Ganaura / Acre > 500, NA, Ganaura),
+         CropOrgFYM = ifelse(CropOrgFYM / Acre > 500, NA, CropOrgFYM),
+         Harv_date = as.Date(ifelse(format(df$Harv_date, "%Y") == "2001",
+                                    Harv_date + 365, Harv_date), origin = "1970-01-01"))
  
- df <- df %>% mutate(mapply(function(x, methods_vec) {
-  output <- as.logical(methods_vec %in% x)
-  names(output) <- paste("OF", methods_vec, sep = "_")
-  output}, strsplit(OrgFertilizers, " "),
-  MoreArgs = list(methods_vec = unique(unlist(strsplit(OrgFertilizers, " "))))) %>%
-   t() %>% as_tibble())
- 
- df <- df %>% mutate(mapply(function(x, methods_vec) {
-  output <- as.logical(methods_vec %in% x)
-  names(output) <- paste("CF", methods_vec, sep = "_")
-  output}, strsplit(CropbasalFerts, " "),
-  MoreArgs = list(methods_vec = unique(unlist(strsplit(CropbasalFerts, " "))))) %>%
-   t() %>% as_tibble())
- 
- df <- df %>% mutate(mapply(function(x, methods_vec) {
-  output <- as.logical(methods_vec %in% x)
-  names(output) <- paste("FTDF", methods_vec, sep = "_")
-  output}, strsplit(FirstTopDressFert, " "),
-  MoreArgs = list(methods_vec = unique(unlist(strsplit(FirstTopDressFert, " "))))) %>%
-   t() %>% as_tibble())
  
  ## replace NAs##
  df <- df %>%
-  mutate(Ganaura = ifelse(OF_Ganaura == 0, 0, Ganaura),
-         CropOrgFYM = ifelse(OF_FYM == 0, 0, CropOrgFYM),
-         BasalUrea = ifelse(CF_Urea == 0, 0, BasalUrea),
-         BasalDAP = ifelse(CF_DAP == 0, 0, BasalDAP),
-         X1tdUrea = ifelse(FTDF_Urea == 0, 0, X1tdUrea),
+  mutate(Ganaura = ifelse(OrgFertilizers_Ganaura == 0, 0, Ganaura),
+         CropOrgFYM = ifelse(OrgFertilizers_FYM == 0, 0, CropOrgFYM),
+         BasalUrea = ifelse(CropbasalFerts_Urea == 0, 0, BasalUrea),
+         BasalDAP = ifelse(CropbasalFerts_DAP == 0, 0, BasalDAP),
+         X1tdUrea = ifelse(FirstTopDressFert_Urea == 0, 0, X1tdUrea),
          X2tdUrea = ifelse(NoFertilizerAppln < 3, 0, X2tdUrea))
  # no third dose of chemical fertilizer was applied
  ftable(is.na(df$X2tdUrea), df$NoFertilizerAppln)
  
  df <- df %>%
-  mutate(X1appDaysUrea = ifelse(FTDF_Urea == 0, -1, X1appDaysUrea),
+  mutate(X1appDaysUrea = ifelse(FirstTopDressFert_Urea == 0, -1, X1appDaysUrea),
          X2appDaysUrea = ifelse(NoFertilizerAppln < 3, -1, X2appDaysUrea))
  
- ## adjust wrong values ##
  df <- df %>%
-  mutate(X1appDaysUrea = ifelse(X1appDaysUrea == 332, NA, X1appDaysUrea),
-         SeedlingsPerPit = ifelse(SeedlingsPerPit == 442, NA, SeedlingsPerPit),
-         Harv_hand_rent = ifelse(Harv_hand_rent == 60000, NA, Harv_hand_rent))
+  rows_update(tibble(ID = df$ID[is.na(df$X2tdUrea) & df$X1tdUrea == 0 & df$X1appDaysUrea == -1],
+                     X2tdUrea = 0, X2appDaysUrea = -1), by = "ID")
+ df <- df %>%
+  rows_update(tibble(ID = df$ID[is.na(df$X2appDaysUrea)],
+                     X2appDaysUrea = mapply(function(dt, dn, dh, maxd) {
+                      max_pos <- as.numeric(max(dh - dt, dh - dn, na.rm = TRUE))
+                      runif(1, 1, min(max_pos, maxd))
+                     }, dt = df[is.na(df$X2appDaysUrea),]$CropTillageDate,
+                     dn = df[is.na(df$X2appDaysUrea),]$RcNursEstDate,
+                     dh = df[is.na(df$X2appDaysUrea),]$Harv_date,
+                     MoreArgs = list(maxd = max(df$X2appDaysUrea, na.rm = TRUE)))), by = "ID")
+ 
+ df = df %>%
+  rows_update(tibble(ID = "ID_YTZN9FE7PQUY",
+                     Harv_date = df$RcNursEstDate[[3134]] + runif(1, 95, 151)), by = "ID")
  
  ## new variables ##
  df <- df %>%
-  mutate(CropCultPerc = CropCultLand / CultLand,
-         TIrriCost_H_Acre = TransIrriCost / (TransplantingIrrigationHours * Acre))
+  mutate(CropCultPerc = CropCultLand / CultLand)
  
- ## adjust variables for area of land under cultivation (Acre) ##
  df <- df %>%
-  mutate(X1tdUrea = X1tdUrea / Acre, X2tdUrea = X2tdUrea / Acre,
-         Harv_hand_rent = Harv_hand_rent / Acre, BasalDAP = BasalDAP / Acre,
-         BasalUrea = BasalUrea / Acre, Ganaura = Ganaura / Acre,
-         CropOrgFYM = CropOrgFYM / Acre)
- 
- data = df
+  rowwise() %>%
+  mutate(NoOrgFerts = sum(c_across(starts_with("OrgFertilizers"))),
+         NoLPMethods = sum(c_across(starts_with("LandPreparationMethod"))),
+         NoNDFactors = sum(c_across(starts_with("NursDetFactor"))),
+         NoTDFactors = sum(c_across(starts_with("TransDetFactor"))),
+         NoCropbFerts = sum(c_across(starts_with("CropbasalFerts"))),
+         NoFTDFerts = sum(c_across(starts_with("FirstTopDressFert"))))
 
- data$CropTillage_Month = month(data$CropTillageDate)
- data$RcNursEst_Month = month(data$RcNursEstDate)
- data$SeedingSowingTransplanting_Month = month(data$SeedingSowingTransplanting)
- data$Harv_Month = month(data$Harv_date)
- data$Threshing_Month = month(data$Threshing_date)
+ df$CropTillage_Month = month(df$CropTillageDate)
+ df$RcNursEst_Month = month(df$RcNursEstDate)
+ df$SeedingSowingTransplanting_Month = month(df$SeedingSowingTransplanting)
+ df$Harv_Month = month(df$Harv_date)
+ df$Threshing_Month = month(df$Threshing_date)
  
- data$CropTillage_Quarter = quarter(data$CropTillageDate)
- data$RcNursEst_Quarter = quarter(data$RcNursEstDate)
- data$SeedingSowingTransplanting_Quarter = quarter(data$SeedingSowingTransplanting)
- data$Harv_Quarter = quarter(data$Harv_date)
- data$Threshing_Quarter = quarter(data$Threshing_date)
+ df$CropTillage_Quarter = quarter(df$CropTillageDate)
+ df$RcNursEst_Quarter = quarter(df$RcNursEstDate)
+ df$SeedingSowingTransplanting_Quarter = quarter(df$SeedingSowingTransplanting)
+ df$Harv_Quarter = quarter(df$Harv_date)
+ df$Threshing_Quarter = quarter(df$Threshing_date)
  
- data$CropTillage_Month_sin = sin(2 * pi * data$CropTillage_Month / 12)
- data$CropTillage_Month_cos = cos(2 * pi * data$CropTillage_Month / 12)
- data$RcNursEst_Month_sin = sin(2 * pi * data$RcNursEst_Month / 12)
- data$RcNursEst_Month_cos = cos(2 * pi * data$RcNursEst_Month / 12)
- data$SeedingSowingTransplanting_Month_sin = sin(2 * pi * data$SeedingSowingTransplanting_Month / 12)
- data$SeedingSowingTransplanting_Month_cos = cos(2 * pi * data$SeedingSowingTransplanting_Month / 12)
- data$Harv_Month_sin = sin(2 * pi * data$Harv_Month / 12)
- data$Harv_Month_cos = cos(2 * pi * data$Harv_Month / 12)
- data$Threshing_Month_sin = sin(2 * pi * data$Threshing_Month / 12)
- data$Threshing_Month_cos = cos(2 * pi * data$Threshing_Month / 12)
+ df$CropTillage_Month_sin = sin(2 * pi * df$CropTillage_Month / 12)
+ df$CropTillage_Month_cos = cos(2 * pi * df$CropTillage_Month / 12)
+ df$RcNursEst_Month_sin = sin(2 * pi * df$RcNursEst_Month / 12)
+ df$RcNursEst_Month_cos = cos(2 * pi * df$RcNursEst_Month / 12)
+ df$SeedingSowingTransplanting_Month_sin = sin(2 * pi * df$SeedingSowingTransplanting_Month / 12)
+ df$SeedingSowingTransplanting_Month_cos = cos(2 * pi * df$SeedingSowingTransplanting_Month / 12)
+ df$Harv_Month_sin = sin(2 * pi * df$Harv_Month / 12)
+ df$Harv_Month_cos = cos(2 * pi * df$Harv_Month / 12)
+ df$Threshing_Month_sin = sin(2 * pi * df$Threshing_Month / 12)
+ df$Threshing_Month_cos = cos(2 * pi * df$Threshing_Month / 12)
  
- data$DiffCropRC = as.numeric(difftime(data$CropTillageDate, data$RcNursEstDate, units = "days"))
- data$DiffCropSeed = as.numeric(difftime(data$CropTillageDate, data$SeedingSowingTransplanting, units = "days"))
- data$DiffCropHarv = as.numeric(difftime(data$CropTillageDate, data$Harv_date, units = "days"))
- data$DiffCropThresing = as.numeric(difftime(data$CropTillageDate, data$Threshing_date, units = "days"))
+ df$DiffCropRC = as.numeric(difftime(df$CropTillageDate, df$RcNursEstDate, units = "days"))
+ df$DiffCropSeed = as.numeric(difftime(df$CropTillageDate, df$SeedingSowingTransplanting, units = "days"))
+ df$DiffCropHarv = as.numeric(difftime(df$CropTillageDate, df$Harv_date, units = "days"))
+ df$DiffCropThresing = as.numeric(difftime(df$CropTillageDate, df$Threshing_date, units = "days"))
  
- data$DiffRCSeed = as.numeric(difftime(data$RcNursEstDate, data$SeedingSowingTransplanting, units = "days"))
- data$DiffRCHarv = as.numeric(difftime(data$RcNursEstDate, data$Harv_date, units = "days"))
- data$DiffRCThresing = as.numeric(difftime(data$RcNursEstDate, data$Threshing_date, units = "days"))
+ df$DiffRCSeed = as.numeric(difftime(df$RcNursEstDate, df$SeedingSowingTransplanting, units = "days"))
+ df$DiffRCHarv = as.numeric(difftime(df$RcNursEstDate, df$Harv_date, units = "days"))
+ df$DiffRCThresing = as.numeric(difftime(df$RcNursEstDate, df$Threshing_date, units = "days"))
  
- data$DiffSeedHarv = as.numeric(difftime(data$SeedingSowingTransplanting, data$Harv_date, units = "days"))
- data$DiffSeedThresing = as.numeric(difftime(data$SeedingSowingTransplanting, data$Threshing_date, units = "days"))
+ df$DiffSeedHarv = as.numeric(difftime(df$SeedingSowingTransplanting, df$Harv_date, units = "days"))
+ df$DiffSeedThresing = as.numeric(difftime(df$SeedingSowingTransplanting, df$Threshing_date, units = "days"))
  
- data$DiffHarvThresing = as.numeric(difftime(data$Harv_date, data$Threshing_date, units = "days"))
+ df$DiffHarvThresing = as.numeric(difftime(df$Harv_date, df$Threshing_date, units = "days"))
  
- data$CropTillageDate = as.numeric(difftime(data$CropTillageDate, min(data$CropTillageDate), units = "days"))
- data$RcNursEstDate = as.numeric(difftime(data$RcNursEstDate, min(data$RcNursEstDate, na.rm = TRUE), units = "days"))
- data$SeedingSowingTransplanting = as.numeric(difftime(data$SeedingSowingTransplanting, min(data$SeedingSowingTransplanting), units = "days"))
- data$Harv_date = as.numeric(difftime(data$Harv_date, min(data$Harv_date), units = "days"))
- data$Threshing_date = as.numeric(difftime(data$Threshing_date, min(data$Threshing_date), units = "days"))
+ df$CropTillageDate = as.numeric(difftime(df$CropTillageDate, min(df$CropTillageDate), units = "days"))
+ df$RcNursEstDate = as.numeric(difftime(df$RcNursEstDate, min(df$RcNursEstDate, na.rm = TRUE), units = "days"))
+ df$SeedingSowingTransplanting = as.numeric(difftime(df$SeedingSowingTransplanting, min(df$SeedingSowingTransplanting), units = "days"))
+ df$Harv_date = as.numeric(difftime(df$Harv_date, min(df$Harv_date), units = "days"))
+ df$Threshing_date = as.numeric(difftime(df$Threshing_date, min(df$Threshing_date), units = "days"))
  
- #data = data %>% mutate(Yield = case_when(Yield > 1000 ~ 1000,
+ #df = df %>% mutate(Yield = case_when(Yield > 1000 ~ 1000,
  #                                         Yield <= 1000 ~ Yield))
  
- return(data)
+ return(df)
 }
 
 # Laden Sie die benötigten Pakete
