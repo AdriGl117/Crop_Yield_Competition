fd917a03eaa19ac1cf405911440398ebef6c78b0
Adrian Glos <adrian@Adrians-MBP.fritz.box>
2023-12-07 08:44:49 +0100

    Catboost learner incl. tuner

--- run_learner.R
+++ run_learner.R
@@ -2,54 +2,52 @@ library(mlr3verse)
 library(dplyr)
 library(lubridate)
 library(mlr3mbo)
-library(ROSE)
-source("get_data.R")
+library(mlr3learners.catboost)
 
-df = get_data(src = "data/Train.csv")
-df[] <- lapply(df, function(x) {
- if(is.character(x)) as.factor(x) else x
-})
-
-df = df %>% mutate(Outlier = ifelse(Yield >= 4000, 1, 0))
-df <- ovun.sample(Outlier ~ ., data = df, 
-                  method = "over")$data
-df = df %>% select(-Outlier)
+df = df %>% subset(Yield/Acre <= 6000)
 
 task = as_task_regr(df, target = "Yield", id = "task")
+task$set_col_roles("ID", add_to = "name", remove_from = "feature")
 
 po_hist = po("imputehist")
-po_mean = po("imputemean")
-po_median = po("imputemedian")
 
-learner = lrn("regr.ranger",
-              mtry = 13,
-              num.trees = 500,
-              max.depth = 300
-              )
+learner = lrn("regr.catboost",
+              iterations = to_tune(1, 1000),
+              learning_rate = to_tune(0.001, 0.1),
+              depth = to_tune(1, 10)
+)
 
-learner_hist = as_learner(po_hist %>>% learner)
-learner_mean = as_learner(po_mean %>>% learner)
-learner_median = as_learner(po_median %>>% learner)
+learner = as_learner(po_hist %>>% learner)
 
-g_ppl = ppl("targettrafo", graph = learner_hist)
-g_ppl$param_set$values$targetmutate.trafo = function(x) log(x)
-g_ppl$param_set$values$targetmutate.inverter = function(x) list(response = exp(x$response))
+#rr = tune_nested(
+# tuner = tnr("mbo"),
+# learner = learner,
+# task = task,
+# inner_resampling = rsmp ("holdout"),
+# outer_resampling = rsmp("cv", folds = 5),
+# measure = msr("regr.rmse"),
+# term_evals = 20)
 
-lrns = c(learner_hist, learner_mean, learner_median, g_ppl)
+#learner = lrn("regr.catboost",
+#              iterations = 965,
+#              learning_rate = 0.08948363,
+#              depth = 6)
 
-resampling = rsmp("cv", folds = 5)
+#learner$train(task)
 
-#Define the Benchmark Grid
-d = benchmark_grid(task = task, learner = lrns, resampling = resampling)
-#Run the Benchmark
-bmr = benchmark(design = d)
-rmse = bmr$aggregate(msr("regr.rmse"))
-rmse
+at = auto_tuner(
+ tuner = tnr("mbo"),
+ learner = learner,
+ resampling = rsmp ("cv", folds = 3),
+ measure = msr("regr.rmse"),
+ term_evals = 30)
 
-# run experiments
-rr1 = resample(task = task, learner = learner_mean, resampling = resampling)
+at$train(task)
+pr = at$predict_newdata(newdata = tdf)
+Yield = round(pr$response, digits = 2)
+ID = tdf$ID
+PR = cbind(ID, Yield)
 
-# access results
-rmse_mean <- rr1$score(msr("regr.rmse"))[, .(task_id, learner_id, iteration, regr.rmse)]
-rmse_mean
-mean(rmse$regr.rmse)
+write.table(PR, "~/Desktop/Predicts.csv", row.names = FALSE, col.names = TRUE, sep = ",", quote = FALSE)
+
+
