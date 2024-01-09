00ac36499ef297050886c915306bb4f3ead5b8b3
AdriGl117 <148889279+AdriGl117@users.noreply.github.com>
2024-01-09 12:09:27 +0100

    Update feature_selection.R

--- feature_selection.R
+++ feature_selection.R
@@ -1,15 +1,36 @@
 library(mlr3fselect)
+library(mlr3filters)
+library(genalg)
 
+# Information Gain
+flt_gain = flt("information_gain")
+flt_gain$calculate(task)
+as.data.table(flt_gain)
+
+# Pearson Correlation
+flt_perm = flt("permutation")
+flt_perm$param_set
+flt_perm$calculate(task)
+
+# Feature Importance
+ttask = task
+lrn_ranger = lrn("regr.ranger", importance = "impurity")
+ttask$filter(ttask$row_ids[complete.cases(ttask$data())])
+flt_importance = flt("importance", learner = lrn_ranger)
+flt_importance$calculate(ttask)
+as.data.table(flt_importance)
+
+# Feature Selection
 instance = fsi(
  task = task,
  learner = as_learner(po("imputehist") %>>% lrn("regr.catboost")),
  resampling = rsmp("cv", folds = 3),
  measures = msr("regr.rmse"),
- terminator = trm("evals", n_evals = 200)
+ terminator = trm("evals", n_evals = 50)
 )
 instance
 
-fselector = fs("random_search", batch_size = 5)
+fselector = fs("genetic_search")
 fselector
 
 fselector$optimize(instance)
@@ -17,5 +38,7 @@ fselector$optimize(instance)
 instance$result_feature_set
 
 instance$result_y
+
+dt = as.data.table(instance$archive)
 
 task$select(instance$result_feature_set)
