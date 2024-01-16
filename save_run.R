54dee14a21c98f55f194995e2f60b5e39c5b3a8b
Adrian Glos <adrian@Adrians-MBP.fritz.box>
2024-01-16 15:33:58 +0100

    Script which creates results.RDS

--- save_run.R
+++ save_run.R
@@ -1,15 +1,12 @@
 Date <- Sys.Date()
 Learner <- learner$id
-Hyper_Parameter <- format_row(as.list(as.data.frame(learner$param_set$values)[1, ]))
-Features <- paste(task$feature_names, collapse = ", ")
-resampling_folds <- cv$param_set$values[[1]]
-cv_score <- rmse
-comment <- "Hyperparameters optimized by Bayesian Optimization with nested resampling"
-results <- data.frame(Date,Features, Learner, Hyper_Parameter, resampling_folds, cv_score, comment)
-
-# Funktion zur Formatierung einer Zeile des Data Frames
-format_row <- function(row) {
- paste(paste(names(row), row, sep = ":"), collapse = ", ")
-}
+Hyper_Parameter <- list(learner$param_set$values)
+Features <- list(task$feature_names)
+Resampling_folds <- cv$param_set$values[[1]]
+CV_Score <- rmse
+Comment <- "Hyperparameters optimized by Bayesian Optimization with nested resampling"
+Imputing <- impute_tech
+Time <- learner$timings[[1]]
+results <- data.table::data.table(Date, Seed, Learner, Hyper_Parameter, Features, Imputing, Time, Resampling_folds, CV_Score, Comment)
 
 saveRDS(results, file = "data/results.RDS")
