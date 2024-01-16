cca73c22493957fd54826e0170bc9b8d59f2b2c2
Adrian Glos <adrian@Adrians-MBP.fritz.box>
2024-01-16 11:36:21 +0100

    update feature_selection

--- feature_selection.R
+++ feature_selection.R
@@ -1,21 +1,42 @@
 library(mlr3fselect)
+library(mlr3filters)
+library(genalg)
 
+task = as_task_regr(df, target = "Yield", id = "task")
+task$set_col_roles("ID", add_to = "name", remove_from = "feature")
+
+# Information Gain
+flt_gain = flt("information_gain")
+flt_gain$calculate(task)
+as.data.table(flt_gain)
+
+# Pearson Correlation
+flt_perm = flt("permutation")
+flt_perm$param_set
+flt_perm$calculate(task)
+
+# Feature Importance
+ttask = task
+lrn_ranger = lrn("regr.ranger", importance = "impurity")
+ttask$filter(ttask$row_ids[complete.cases(ttask$data())])
+flt_importance = flt("importance", learner = lrn_ranger)
+flt_importance$calculate(ttask)
+as.data.table(flt_importance)
+
+# Feature Selection
 instance = fsi(
  task = task,
- learner = as_learner(po("imputehist") %>>% lrn("regr.catboost")),
+ learner = as_learner(po("imputehist") %>>% lrn("regr.ranger")),
  resampling = rsmp("cv", folds = 3),
  measures = msr("regr.rmse"),
- terminator = trm("evals", n_evals = 200)
+ terminator = trm("evals", n_evals = 50)
 )
-instance
 
-fselector = fs("random_search", batch_size = 5)
-fselector
+fselector = fs("rfe")
 
 fselector$optimize(instance)
 
-instance$result_feature_set
+saveRDS(c(instance$result_feature_set, "ID"), "data/feature_list.RDS")
 
-instance$result_y
+dt = as.data.table(instance$archive)
 
-task$select(instance$result_feature_set)
