35ea47bec45d364352326a34088e0c464b6790c8
Adrian Glos <adrian@Adrians-MBP.fritz.box>
2024-03-22 17:04:37 +0100

    Final Script for Feature Selection

--- feature_selection.R
+++ feature_selection.R
@@ -4,16 +4,16 @@ library(genalg)
 
 # Information Gain
 flt_gain = flt("information_gain")
-flt_gain$calculate(task)
+flt_gain$calculate(task_All)
 as.data.table(flt_gain)
 
 # Pearson Correlation
 flt_perm = flt("permutation")
 flt_perm$param_set
-flt_perm$calculate(task)
+flt_perm$calculate(task_All)
 
 # Feature Importance
-ttask = task
+ttask = task_All
 lrn_ranger = lrn("regr.ranger", importance = "impurity")
 ttask$filter(ttask$row_ids[complete.cases(ttask$data())])
 flt_importance = flt("importance", learner = lrn_ranger)
@@ -26,8 +26,7 @@ instance_ranger = fselect(
  task = task_All,
  learner = lrn("regr.ranger"),
  resampling = rsmp("cv", folds = 3),
- measures = msr("regr.rmse")#,
- #terminator = trm("evals", n_evals = 50)
+ measures = msr("regr.rmse")
 )
 
 autoplot(instance_ranger, type = "performance") + 
@@ -36,30 +35,17 @@ autoplot(instance_ranger, type = "performance") +
  theme(plot.title = element_text(hjust = 0.5, size=40)) +
  theme(legend.text = element_text(size = 20))
 
-#fselector = fs("sequential")
-
-#fselector$optimize(instance_ranger)
-
 saveRDS(c(instance_ranger$result_feature_set),
         "features/feature_list_ranger.RDS")
 
 instance_catboost = fselect(
- fselector = fs("sequential", strategy = "sfs"), #sfs forward, sbs backwards
+ fselector = fs("sequential", strategy = "sbs"), #sfs forward, sbs backwards
  task = task_All,
  learner = lrn("regr.catboost",
                thread_count = 6),
  resampling = rsmp("cv", folds = 3),
- measures = msr("regr.rmse")#,
- #terminator = trm("evals", n_evals = 1000)
+ measures = msr("regr.rmse")
 )
 
-#fselector = fs("genetic_search")#genetic_search
-
-#fselector$optimize(instance_catboost)
-
 saveRDS(c(instance_catboost$result_feature_set),#, "ID"), 
-        "features/feature_list_catboost_vaishali.RDS")
-
-#dt = as.data.table(instance$archive)
-
-#task$select(instance$result_feature_set)
+        "features/feature_list_catboost_sequential.RDS")
